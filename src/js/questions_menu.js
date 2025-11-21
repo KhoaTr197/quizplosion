2e265c208dbf442ded5081b506c15dea1c42176b
Huy Huynh <hkhuy2208@gmail.com>
2025-11-22 00:52:39 +0700

    feat: đổi tên class question-card -> question-item

--- src/js/questions_menu.js
+++ src/js/questions_menu.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const questionCards = document.querySelectorAll('.question-card');
+    const questionCards = document.querySelectorAll('.question-item');
 
     questionCards.forEach(card => {
         card.addEventListener('click', function() {
             console.log('Question card clicked:', this);
-            this.classList.add('question-card--hidden');
+            this.classList.add('question-item--hidden');
         });
     });
 });
